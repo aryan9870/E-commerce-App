f7147c12727ee2c7cdad1fc74ebe50038d337080
Aryan <aryan7017n@gmail.com>
2026-03-07 16:43:40 +0530

    implement removeFromCart and updateCartQuantity APIs

--- server/controllers/cartController.js
+++ server/controllers/cartController.js
@@ -9,7 +9,7 @@ export const addToCart = async (req, res, next) => {
     const { productId } = req.body;
 
     if(!productId) {
-        return next(new ErrorHandler("Product is required", 404));
+        return next(new ErrorHandler("Product is required", 400));
     }
   
     // find user's cart
@@ -76,6 +76,84 @@ export const getCart = async (req, res, next) => {
       success: true,
       cart,
     });
+};
+
+// Update quantity of a product in the user's cart (+1 or -1 only)
+export const updateCartQuantity = async (req, res, next) => {
+  const userId = req.user._id;
+  const { productId } = req.params;
+  const { operation } = req.body; // operation: 'increment' or 'decrement'
+
+  if (!productId) {
+    return next(new ErrorHandler("Product ID is required", 400));
+  }
+  if (!["increment", "decrement"].includes(operation)) {
+    return next(
+      new ErrorHandler("Valid operation required: 'increment' or 'decrement'", 400)
+    );
+  }
+
+  const cart = await Cart.findOne({ user: userId });
+  if (!cart) {
+    return next(new ErrorHandler("Cart not found", 404));
+  }
+
+  const item = cart.items.find(
+    (item) => item.product.toString() === productId
+  );
+  if (!item) {
+    return next(new ErrorHandler("Product not found in cart", 404));
+  }
+
+  if (operation === "increment") {
+    item.quantity += 1;
+  } else if (operation === "decrement") {
+    if (item.quantity > 1) {
+      item.quantity -= 1;
+    } else {
+      // If quantity is 1 and user tries to decrement, remove the item
+      cart.items = cart.items.filter(
+        (i) => i.product.toString() !== productId
+      );
+    }
+  }
+
+  await cart.save();
+
+  res.status(200).json({
+    success: true,
+    message: "Cart quantity updated",
+    cart,
+  });
+};
+
+
+
+// Remove item from cart
+export const removeFromCart = async (req, res, next) => {
+  const userId = req.user._id;
+  const { productId } = req.params;
+
+  if (!productId) {
+    return next(new ErrorHandler("Product ID is required", 400));
+  }
+
+  const cart = await Cart.findOne({ user: userId });
+  if (!cart) {
+    return next(new ErrorHandler("Cart not found", 404));
+  }
+
+  cart.items = cart.items.filter(
+    (item) => item.product.toString() !== productId
+  );
+
+  await cart.save();
+
+  res.status(200).json({
+    success: true,
+    message: "Product removed from cart",
+    cart,
+  });
 };
 
 
--- server/routes/cartRoute.js
+++ server/routes/cartRoute.js
@@ -1,10 +1,11 @@
 import express from "express";
 const router = express.Router();
-import { addToCart, getCart } from "../controllers/cartController.js";
+import { addToCart, getCart, removeFromCart, updateCartQuantity } from "../controllers/cartController.js";
 import { isLoggedIn } from "../middlewares/authMiddleware.js";
 
 router.get("/", isLoggedIn, getCart);
 router.post("/", isLoggedIn, addToCart);
-
+router.put("/:productId", isLoggedIn, updateCartQuantity);
+router.delete("/:productId", isLoggedIn, removeFromCart);
 
 export default router;

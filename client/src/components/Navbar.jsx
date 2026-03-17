abe182caff5ca9829c65ae3c1e7d4d989f02ac17
Aryan <aryan7017n@gmail.com>
2026-03-17 12:39:31 +0530

    connect logout api to frontend

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -6,11 +6,41 @@ import { NavLink } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
 import { FiLogIn } from "react-icons/fi";
+import { FaRegUser } from "react-icons/fa";
+import { HiOutlineShoppingBag } from "react-icons/hi";
 import { FaBars } from "react-icons/fa6";
+import useAuthStore from "../store/useAuthStore";
+import axios from "axios";
+import toast from "react-hot-toast";
 
 const Navbar = () => {
   const navigate = useNavigate();
   const [open, setOpen] = useState(false);
+  const isLoggedIn = useAuthStore((state) => state.isLoggedIn);
+  const logout = useAuthStore((state) => state.logout);
+  const API_URL = import.meta.env.VITE_API_URL;
+
+  const handleLogout = async () => {
+    try {
+      const res = await axios.get(
+        `${API_URL}/users/logout`,
+        { withCredentials: true },
+      );
+      if(res.data.success === true){
+        logout();
+        toast.success(res.data.message);
+        setOpen(false);
+      }
+    } catch (error) {
+      console.log(error.response);
+      if(error.response.data.errors){
+        toast.error(error.response.data.errors[0].message);
+      }
+      else{
+        toast.error(error.response.data.message);
+      }
+    }
+  };
 
   return (
     <div className="px-20 fixed top-0 left-0 right-0 z-100 bg-white max-sm:px-5 text-gray-600 tracking-wider">
@@ -70,34 +100,47 @@ const Navbar = () => {
             onClick={() => navigate("/cart")}
             className="text-2xl cursor-pointer"
           />
-          <span className="hidden">
-            <LuCircleUserRound
-              onClick={() => setOpen(!open)}
-              className="text-2xl cursor-pointer"
-            />
-          </span>
-          <NavLink to="/login" className="bg-black text-white px-3 py-1 rounded-sm hover:bg-gray-800 transition">
-            Login
-          </NavLink>
-          {/* Dropdown Menu */}
-          {open && (
-            <div className="absolute top-20 right-20 w-40 bg-white shadow-sm rounded max-sm:right-10">
-              <ul className="py-2">
-                <li
-                  onClick={() => navigate("/login")}
-                  className="px-4 py-2 hover:bg-gray-200 cursor-pointer flex items-center justify-around"
-                >
-                  <span>Sign In</span> <FiLogIn />
-                </li>
 
-                <li
-                  onClick={() => navigate("/signup")}
-                  className="px-4 py-2 hover:bg-gray-200 cursor-pointer flex items-center justify-around"
-                >
-                  <span>Sign Up</span> <FiLogIn />
-                </li>
-              </ul>
-            </div>
+          {isLoggedIn ? (
+            <span>
+              <LuCircleUserRound
+                onClick={() => setOpen(!open)}
+                className="text-2xl cursor-pointer"
+              />
+              {/* Dropdown Menu */}
+              {open && (
+                <div className="absolute top-20 right-20 w-40 bg-white shadow-sm rounded max-sm:right-10">
+                  <ul className="py-2">
+                    <li
+                      onClick={() => navigate("/profile")}
+                      className="px-4 py-2 hover:bg-gray-200 cursor-pointer flex items-center justify-around"
+                    >
+                      <span>Profile</span> <FaRegUser />
+                    </li>
+                    <li
+                      onClick={() => navigate("/orders")}
+                      className="px-4 py-2 hover:bg-gray-200 cursor-pointer flex items-center justify-around"
+                    >
+                      <span>Orders</span> <HiOutlineShoppingBag />
+                    </li>
+                    <li
+                      onClick={handleLogout}
+                      className="px-4 py-2 hover:bg-gray-200 cursor-pointer flex items-center justify-around"
+                    >
+                      <span>Logout</span> <FiLogIn />
+                    </li>
+                    
+                  </ul>
+                </div>
+              )}
+            </span>
+          ) : (
+            <NavLink
+              to="/login"
+              className="bg-black text-white px-3 py-1 rounded-sm hover:bg-gray-800 transition"
+            >
+              Login
+            </NavLink>
           )}
         </div>
       </div>

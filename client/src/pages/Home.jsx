af55df740fd8e4dd9932a5d280678f5b95bdd91a
Aryan <aryan7017n@gmail.com>
2026-03-17 23:11:11 +0530

    fetch products and display on home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,18 +1,40 @@
 import React from 'react'
 import Hero from '../components/Hero'
 import ProductSection from '../components/ProductSection'
-import { products } from '../assets/assets'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Home = () => {
+
+  const API_URL = import.meta.env.VITE_API_URL;
+  const [items, setItems] = useState([]);
+
+  const fetchProducts = async () => {
+    try {
+      const res = await axios.get(`${API_URL}/products`);
+      console.log(res.data.products);
+      setItems(res.data.products);
+    } catch (error) {
+      console.log(error.response.data.message);
+    }
+  }
+
+  useEffect(() => {
+    fetchProducts();
+  }, [])
+
+  let newArrivals = items.slice(0, 4);
+  let topSelling = items.filter(item => item.ratings >= 4).slice(0, 4);
+
   return (
     <div className='text-gray-600 tracking-wider'>
       <Hero />
       <div className='mx-20 max-sm:mx-5'>
-      <ProductSection title="NEW ARRIVALS" products={products} />
+      <ProductSection title="NEW ARRIVALS" products={newArrivals} />
       </div>
       <div className='border border-gray-200 mx-20 my-10 max-sm:mx-5 max-sm:my-5'></div>
       <div className='mx-20 max-sm:mx-5'>
-      <ProductSection title="TOP SELLING" products={products} />
+      <ProductSection title="TOP SELLING" products={topSelling} />
       </div>
     </div>
   )
